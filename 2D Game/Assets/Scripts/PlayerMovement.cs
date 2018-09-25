3ea178c3b6462f15c6d1cc6f84826e936553099b
BrianD147 <briandoyle147@gmail.com>
2018-09-25 18:10:27 +0100

    Testing touch input

---
+++ 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.touchCount > 0)
+        {
+            //Test touch input
+            Debug.Log(Input.GetTouch(0).position);
+        }
+	}
+	
+}
