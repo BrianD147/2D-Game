6d20563b1c4f777a53c6b00064271a8fea5a6bc5
BrianD147 <briandoyle147@gmail.com>
2018-09-27 15:30:03 +0100

    Testing touch jump mechanics

---
+++ 2D Game/Assets/Scripts/Jump.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    [SerializeField]
+    private float jumpVelocity;
+
+    [SerializeField]
+    private float fallMultiplyer;
+
+    private bool isGrounded;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate () {
+        //Debug.Log(isGrounded);
+        //Debug.Log(GetComponent<Rigidbody2D>().velocity.y);
+
+        //Increase gravity after apex of jump
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplyer - 1) * Time.deltaTime;
+        }
+
+        //When the screen is touched
+        if (Input.touchCount > 0)
+        {
+            if (isGrounded)
+            {
+                rb.velocity = Vector2.up * jumpVelocity;
+                isGrounded = false;
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Grounded");
+        isGrounded = true;
+    }
+}
--- 2D Game/Assets/Scripts/PlayerMovement.cs
+++ 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,44 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    // Update is called once per frame
+    private float fallMultiplyer = 1f;
+    private float lowJumpMultiplyer = 2f;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+
     void Update () {
         if (Input.touchCount > 0)
         {
             //Test touch input
-            Debug.Log(Input.GetTouch(0).position);
+            //Debug.Log(Input.GetTouch(0).position);
         }
 	}
-	
+
+    private void FixedUpdate()
+    {
+        //rb.velocity = new Vector2(speed, 0f);
+        Debug.Log(Physics.Raycast(transform.position, Vector2.down, 0.5f));
+
+        if (rb.velocity.y < 0)
+        {
+            //Debug.Log("Falling");
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplyer - 1) * Time.deltaTime;
+        } else if(rb.velocity.y >= 0 && !Input.GetKeyDown (KeyCode.Space))
+        {
+            //Debug.Log("Low Jump");
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplyer - 1) * Time.deltaTime;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector2.down, 0.5f);
+    }
+
 }
